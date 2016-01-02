c9b2dd5f18568b723d6a70cc453163a8be0bb39f
JV Estolas <fortis201@gmail.com>
2016-01-01 19:18:06 -0800

    minor change in server.js

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var bodyParser = require('body-parser');
 app.use(express.static(path.join(__dirname, './app')));
 
 //must be before routes.js!!
-// app.use(bodyParser.urlencoded());
+
 app.use(bodyParser.json());
 
 // require('./server/config/mongoose.js');
 // require('./server/config/routes.js')(app);
 
-// app.get('/', function (req, res) {
-// 	res.render('index');
-// })
+app.get('/', function (req, res) {
+	res.render('index');
+})
 
 var server = app.listen(1232, function () {
 	console.log('Established connection to 1232.');
